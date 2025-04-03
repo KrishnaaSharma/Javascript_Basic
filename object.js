321e93ab9750eb05e15300759005d089150c99d3
KrishnaaSharma <krishnaasharma647@gmail.com>
2025-04-03 14:57:40 +0530

    object

--- object.js
+++ object.js
@@ -1,44 +1,30 @@
-console.log('---- Creating Object in different way ----')
 
-console.log('---- 1. Using Object literals (Most Common Way) -----')
-let avenger = {
-    f_name: 'Steve',                     // properties
-    l_name: 'Roger',                    //  properties
-    'heroic name': 'Captain America',    //   properties
-    power: function () { return 'I am the first avenger' }   // This is called method
+let person= {
+    f_name : 'steve',
+    l_name: 'roger',
+    'heroic_name': 'Captain American',
+    start: function (){
+        return 'I am avengers'
+    }
 }
-// All these f_name,l_name,heroic_name are called properties of the object.
 
-// let marvel = {f_name: 'Steve',l_name: 'Roger',heroic_name: 'Captain America'} you can also write in single line.
+console.log(person)
+console.log(person.f_name)
+console.log(person['heroic_name'])
+console.log(person.start()) // I am avengers
 
-// Accessing properties and method
-console.log(avenger)
-console.log(avenger.f_name)
-console.log(avenger['heroic name'])
-console.log(avenger.power())
 
-// Assigning new value to the properties and method
-avenger.f_name = 'Steven'
-avenger.power = function () { return 'I am now an avenger !' }
-console.log(avenger)
-console.log(avenger.power())
+person.f_name='stevan'
+person.start =function () { return'i am now avengers'}
 
-// Deleting properties and method in objects.
+// update new value
 
-delete avenger.l_name
-delete avenger.power
+console.log(person)
+console.log(person.start())
 
-console.log(avenger)
-// =========================================================
+// delete l_name
 
-console.log('---- 2. Using Object Constructor -----')
+delete person.l_name
+delete person.start
 
-let dc = new Object()      // => is same as let dc= {}
-dc.f_name = 'Bruce'
-dc.l_name = 'Wayne'
-dc['heroic name'] = 'Batman'
-dc['power'] = function(){ return 'My Power is Darkness'}
-
-console.log(dc)
-console.log(dc.l_name)
-console.log(dc['heroic name'])
+console.log(person) // {f_name: 'stevan', heroic_name: 'Captain American'}
